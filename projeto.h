92f3dec65de454e86333783958d604529e6cdf95
Guilherme Figueiredo Castro <a110452@uminho.pt>
2025-04-19 15:00:19 +0100

    Update projeto.h

--- projeto.h
+++ projeto.h
@@ -1,11 +1,27 @@
 #ifndef PROJETO_H
 #define PROJETO_H
 
+// Declarar as funções
 void lerEstadoJogo(char *nomearquivo, char tabuleiro[1000][1000], int *linhas, int *colunas);
 void imprimirTabuleiro(char tabuleiro[1000][1000], int linhas, int colunas);
 void pintarDeBranco(char tabuleiro[1000][1000], int linhas, int colunas, int x, int y);
 void riscar(char tabuleiro[1000][1000], int linhas, int colunas, int x, int y);
 void imprimir_comandos();
 int formatoParaCoordenadas(char *input, int *x, int *y);
+
+// Variaveis globais
+#define STACK_SIZE 100
+typedef struct {
+    char tabuleiro[1000][1000];
+    int linhas;
+    int colunas;
+} Tabuleiro;
+
+extern Tabuleiro stack[STACK_SIZE];
+extern int topoStack;
+
+void stacks(Tabuleiro estado);
+Tabuleiro desempilhar();
+
 
 #endif
